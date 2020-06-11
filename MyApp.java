c14100efc7e318981eef06e40c302c879e81e92c
vagrant <vagrant@localhost.localdomain>
2020-06-11 19:34:53 +0900

    様々な変数

--- MyApp.java
+++ MyApp.java
@@ -1,11 +1,22 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    // 変数
-    // 型 変数名
-    // String msg;
-    // msg = "Hello world Again!";
-    String msg = "Hello world Again!";
+    // 文字
+    char a = 'a';
+    // 整数 byte short int long
+    int x = 10;
+    long y = 55555555L; // longを使う時には最後にLをつける
+    // 不動少数点数 float double
+    double d = 23423.344;
+    float f = 32.33F;
+    // 論理値
+    boolean flag = true; // false
+
+    
+    // 文字列
+    // \n 改行
+    // \t タブ
+    String msg = "Hello wo\nrld Again\t!";
     System.out.println(msg);
   }
 }
