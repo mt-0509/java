afce29ec27f15ff2f23d8d1cfa4d8225fd9aabe3
vagrant <vagrant@localhost.localdomain>
2020-06-12 16:47:32 +0900

    条件分岐 switch文

--- MyApp.java
+++ MyApp.java
@@ -1,21 +1,23 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    // if
-    // > >= < <= == !=
-    // && || !
-    int score = 95;
-    // if (score > 80) {
-    //   System.out.println("Great!");
-    // } else if (score > 60) {
-    //   System.out.println("Good!");
-    // } else {
-    //   System.out.println("so so ... !");
-    // }
-
-    // 条件演算子を使った書き方
-    String msg = score > 80 ? "Great!" : "so so ... !";
-    System.out.println(msg);
+    // switch
+    String signal = "green";
+    switch (signal) {
+      case "red":
+        System.out.println("stop!");
+        break;
+      case "blue":
+      case "green":
+        System.out.println("go!");
+        break;
+      case "yellow":
+        System.out.println("caution!");
+        break;
+      default:
+        System.out.println("wrong signal!");
+      break;
+    }
 
   }
 }
