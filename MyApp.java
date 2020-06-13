60f3224f1911fc9b018e97f20ccc0552c0b88fe8
vagrant <vagrant@localhost.localdomain>
2020-06-13 10:10:36 +0900

    配列

--- MyApp.java
+++ MyApp.java
@@ -1,17 +1,23 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    // for
-    // 0 - 9
-    // break
-    // continue
-    for (int i = 0; i < 10; i++) {
-      if (i == 5) {
-        // break;
-        continue;
-      }
-      System.out.println(i);
-    }
+    // 配列
+    int[] sales; //配列の宣言
+    sales = new int[3]; // intを3つ分確保
+    sales[0] = 100;
+    sales[1] = 200;
+    sales[2] = 300;
+
+    int[] sales;
+    sales = new int[] {100, 200, 300};
+
+    int[] sales = {100, 200, 300};
+    
+    System.out.println(sales[1]); // 200
+
+    sales[1] = 1000;
+
+    System.out.println(sales[1]); // 1000
 
   }
 }
