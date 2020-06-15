943fe056d6295a77bbd2b71b10c9515777825b02
vagrant <vagrant@localhost.localdomain>
2020-06-15 20:29:28 +0900

    メソッドのオーバーロード

--- MyApp.java
+++ MyApp.java
@@ -1,17 +1,20 @@
 public class MyApp {
   // method
-  public static String sayHi(String name) {
-    // System.out.println("Hi!");
-    // System.out.println("Hi! " + name);
-    return "Hi! " + name;
+  public static void sayHi(String name) {
+    // int x = 10;
+    System.out.println("Hi! " + name);
+  }
+
+  // overload
+
+  public static void sayHi() {
+    System.out.println("Hi! Nobody!");
   }
 
   public static void main(String[] args) {
-    // sayHi();
-    // sayHi("Tom");
-    // sayHi("Bob");
-    String msg = sayHi("Steve");
-    System.out.println(msg);
-
+    sayHi(); 
+    sayHi("Steve");
+    // System.out.println(name);
+    // System.out.println(x);
   }
 }
