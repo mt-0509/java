5ed6297afa0410a03e0a4fecc3a415727e2a72b3
vagrant <vagrant@localhost.localdomain>
2020-06-15 20:30:18 +0900

    class

--- MyApp.java
+++ MyApp.java
@@ -1,20 +1,20 @@
+class User {
+  String name = "Me!";
+
+  void sayHi() {
+    System.out.println("hi!");
+  }
+}
+
 public class MyApp {
-  // method
-  public static void sayHi(String name) {
-    // int x = 10;
-    System.out.println("Hi! " + name);
-  }
-
-  // overload
-
-  public static void sayHi() {
-    System.out.println("Hi! Nobody!");
-  }
 
   public static void main(String[] args) {
-    sayHi(); 
-    sayHi("Steve");
-    // System.out.println(name);
-    // System.out.println(x);
+
+    // Class
+    User tom;
+    tom = new User(); // インスタンス
+    
+    System.out.println(tom.name);
+    tom.sayHi();
   }
 }
