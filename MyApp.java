676284f3336136713365a66d196fe5602fd2d0b4
vagrant <vagrant@localhost.localdomain>
2020-06-24 20:49:26 +0900

    抽象クラスについて

--- MyApp.java
+++ MyApp.java
@@ -1,44 +1,30 @@
-// final:変更ができない
+// 抽象クラス -> 具象クラス
 
-final class User {
-  protected String name;
-  private static final double VERSION = 1.1;
+abstract class User {
+  public abstract void sayHi(); // 抽象メソッド
+}
 
-  public User(String name) {
-    this.name = name;
-    User.VERSION = 1.2;
+class JapaneseUser extends User {
+  @Override
+  public void sayHi() {
+    System.out.println("こんにちは！");
   }
+}
 
-  public final void sayHi() {
-    System.out.println("hi! " + this.name);
+class AmericanUser extends User {
+  @Override
+  public void sayHi() {
+    System.out.println("Hi!");
   }
-
-  class Adminuser extends User {
-
-    public Adminuser(String name) {
-      super(name);
-    }
-
-    @Override
-    public void sayHi() {
-      System.out.println("[admin] hi! " + this.name);
-    }
-  }
-
-  public static void getInfo() { // クラスメソッド
-    System.out.println("# of instances: " + User.count);
-  }
-
 }
 
 public class MyApp {
 
   public static void main(String[] args) {
-    User.getInfo(); // 0
-    User tom = new User("tom");
-    User.getInfo(); // 1
-    User bob = new User("bob");
-    User.getInfo(); // 2
+    AmericanUser tom = new AmericanUser();
+    JapaneseUser aki = new JapaneseUser();
+    tom.sayHi();
+    aki.sayHi();
   }
 
 }
