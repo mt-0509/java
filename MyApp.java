3c25e3b1a92ec8ef77cc3227de03ce0fcd274e72
vagrant <vagrant@localhost.localdomain>
2020-06-25 18:43:47 +0900

    例外処理

--- MyApp.java
+++ MyApp.java
@@ -1,28 +1,31 @@
-// 列挙型
+// 例外
 
-enum Result {
-  SUCCESS, // 0
-  ERROR, // 1
+class MyException extends Exception {
+  public MyException(String s) {
+    super(s);
+  }
 }
 
 public class MyApp {
 
-  public static void main(String[] args) {
-    Result res;
-
-    res = Result.ERROR;
-
-    switch (res) {
-      case SUCCESS:
-        System.out.println("OK!");
-        System.out.println(res.ordinal()); // 0
-        break;
-      case ERROR:
-        System.out.println("NG!");
-        System.out.println(res.ordinal()); // 1
-        break;
+  public static void div(int a, int b) {
+    try {
+      if (b < 0) {
+        throw new MyException("not minus!");
+      }
+      System.out.println(a / b);
+    } catch(ArithmeticException e) {
+      System.err.println(e.getMessage());
+    } catch(MyException e) {
+      System.err.println(e.getMessage());
+    } finally {
+      System.out.println(" -- end -- ");
     }
-    
+  }
+
+  public static void main(String[] args) {
+    div(3, 0);
+    div(5, -2);
   }
 
 }
