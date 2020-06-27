adb46fbd6b965a0c9c1a79c8e337d4e60af2d95b
vagrant <vagrant@localhost.localdomain>
2020-06-27 16:59:21 +0900

    無名クラス、ラムダ式

--- MyApp.java
+++ MyApp.java
@@ -1,24 +1,41 @@
 // Thread
 
-class MyRunnable implements Runnable {
-  @Override
-  public void run() {
-    for (int i = 0; i < 500; i++) {
-      System.out.print('*');
-    }
-  }
-}
+// class MyRunnable implements Runnable { // 関数型インターフェース
+//   @Override
+//   public void run() {
+//     for (int i = 0; i < 500; i++) {
+//       System.out.print('*');
+//     }
+//   }
+// }
 
 public class MyApp {
 
   public static void main(String[] args) {
-    MyRunnable r = new MyRunnable();
-    Thread t = new Thread(r);
-    t.start();
-    
+    // MyRunnable r = new MyRunnable();
+    // Thread t = new Thread(r);
+    // t.start();
+
+    // ラムダ式
+    // (引数) -> {処理}
+    // new Thread(new Runnable() {
+    //   @Override
+    //   public void run() {
+    //     for (int i = 0; i < 500; i++) {
+    //       System.out.print('*');
+    //     }
+    //   }
+    // }).start(); // 無名クラス
+
+    new Thread(() -> {
+      for (int i = 0; i < 500; i++) {
+        System.out.print('*');
+      }
+    }).start();
+
     for (int i = 0; i < 500; i++) {
       System.out.print('.');
     }
-    
   }
+
 }
