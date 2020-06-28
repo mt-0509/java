c5b4a708f0892ec404bb61a0a13f4cb40085f638
vagrant <vagrant@localhost.localdomain>
2020-06-28 11:13:49 +0900

    printfメソッド

--- MyApp.java
+++ MyApp.java
@@ -1,41 +1,15 @@
-// Thread
-
-// class MyRunnable implements Runnable { // 関数型インターフェース
-//   @Override
-//   public void run() {
-//     for (int i = 0; i < 500; i++) {
-//       System.out.print('*');
-//     }
-//   }
-// }
-
 public class MyApp {
 
   public static void main(String[] args) {
-    // MyRunnable r = new MyRunnable();
-    // Thread t = new Thread(r);
-    // t.start();
 
-    // ラムダ式
-    // (引数) -> {処理}
-    // new Thread(new Runnable() {
-    //   @Override
-    //   public void run() {
-    //     for (int i = 0; i < 500; i++) {
-    //       System.out.print('*');
-    //     }
-    //   }
-    // }).start(); // 無名クラス
+    int score = 50;
+    double height = 165.8;
+    String name = "taguchi";
 
-    new Thread(() -> {
-      for (int i = 0; i < 500; i++) {
-        System.out.print('*');
-      }
-    }).start();
-
-    for (int i = 0; i < 500; i++) {
-      System.out.print('.');
-    }
+    System.out.printf("name: %s, score: %d, height: %f\n", name, score, height);
+    System.out.printf("name: %-10s, score: %10d, height: %5.2f\n", name, score, height);
+    
+    String s = String.format("name: %10s, score: %-10d, height: %5.2f\n", name, score, height);
+    System.out.println(s);
   }
-
 }
