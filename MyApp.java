7a3ec2c772b38eee26ecb178d6b32a296a214713
vagrant <vagrant@localhost.localdomain>
2020-06-30 21:29:37 +0900

    Stream API

--- MyApp.java
+++ MyApp.java
@@ -3,29 +3,20 @@ import java.util.*;
 public class MyApp {
 
   public static void main(String[] args) {
-    // HashMap: key value
-    // TreeMap
-    // LinkedHashMap
+    // Stram
+    List<Integer> sales = new ArrayList<>(Arrays.asList(12, 30, 22, 4, 9));
+    // for (Integer sale : sales) {
+    //   System.out.println(sale);
+    // }
 
-    // HashMap<String, Integer> sales = new HashMap();
-    Map<String, Integer> sales = new HashMap<>();
+    sales
+      .stream()
+      // 中間処理
+      .filter(e -> e % 3 == 0) // ラムダ式 引数 -> 処理
+      .map(e -> "(" + e + ")")
+      // 終端処理
+      .forEach(System.out::println);
 
-    sales.put("tom", 10);
-    sales.put("bob", 20);
-    sales.put("steve", 30);
-
-    System.out.println(sales.get("tom")); // 10
-    System.out.println(sales.size()); // 3
-
-    for (Map.Entry<String, Integer> sale : sales.entrySet()) {
-      System.out.println(sale.getKey() + ":" + sale.getValue());
-    }
-
-    sales.put("tom", 100);
-    sales.remove("steve");
-
-    for (Map.Entry<String, Integer> sale : sales.entrySet()) {
-      System.out.println(sale.getKey() + ":" + sale.getValue());
-    }
+  
   }
 }
