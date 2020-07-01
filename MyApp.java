73e8c1b73253f23e4ec216dea1665e4b60d4d9d5
vagrant <vagrant@localhost.localdomain>
2020-07-01 20:17:24 +0900

    DateTimeFormatter

--- MyApp.java
+++ MyApp.java
@@ -1,22 +1,20 @@
-import java.util.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class MyApp {
 
   public static void main(String[] args) {
-    // Stram
-    List<Integer> sales = new ArrayList<>(Arrays.asList(12, 30, 22, 4, 9));
-    // for (Integer sale : sales) {
-    //   System.out.println(sale);
-    // }
+    LocalDateTime d = LocalDateTime.now(); // 現在時刻のインスタンス
+    // LocalDateTime d = LocalDateTime.of(2016, 1, 1, 10, 10, 10);
+    // LocalDateTime d = LocalDateTime.parse("2016-01-01T10:10:10");
 
-    sales
-      .stream()
-      // 中間処理
-      .filter(e -> e % 3 == 0) // ラムダ式 引数 -> 処理
-      .map(e -> "(" + e + ")")
-      // 終端処理
-      .forEach(System.out::println);
+    System.out.println(d.getYear());
+    System.out.println(d.getMonth().getValue());
 
-  
+    System.out.println(d.plusMonths(2).minusDays(3));
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy!MM!dd");
+    System.out.println(d.format(dtf));
+
   }
 }
