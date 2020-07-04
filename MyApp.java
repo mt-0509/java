f4ac7149c1c3dc128fb9adaac742e478d0fd3bcd
vagrant <vagrant@localhost.localdomain>
2020-07-04 21:51:41 +0900

    エスケープシーケンス

--- MyApp.java
+++ MyApp.java
@@ -1,13 +1,7 @@
-import java.time.*;
-import java.time.format.DateTimeFormatter;
 
 public class MyApp {
 
   public static void main(String[] args) {
-    boolean eror = true;
-    char moji = '駆';
-    double pi = 3.14;
-    long num = 314159265853979L;
-    String damage = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    System.out.println("私の好きな記号は二重引用符(\")です");
   }
 }
