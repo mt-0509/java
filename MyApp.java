824e11cc6ef902c36d5e541c26246b56bfebc4bd
vagrant <vagrant@localhost.localdomain>
2020-07-04 20:21:42 +0900

    定数の宣言

--- MyApp.java
+++ MyApp.java
@@ -4,17 +4,9 @@ import java.time.format.DateTimeFormatter;
 public class MyApp {
 
   public static void main(String[] args) {
-    LocalDateTime d = LocalDateTime.now(); // 現在時刻のインスタンス
-    // LocalDateTime d = LocalDateTime.of(2016, 1, 1, 10, 10, 10);
-    // LocalDateTime d = LocalDateTime.parse("2016-01-01T10:10:10");
-
-    System.out.println(d.getYear());
-    System.out.println(d.getMonth().getValue());
-
-    System.out.println(d.plusMonths(2).minusDays(3));
-
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy!MM!dd");
-    System.out.println(d.format(dtf));
-
+    final double PI = 3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、 ");
+    System.out.println(pie * pie * PI);
   }
 }
