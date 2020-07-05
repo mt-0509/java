db9eb934c94ee7f3f72de256859239c8465b3782
vagrant <vagrant@localhost.localdomain>
2020-07-05 22:46:21 +0900

    優先順位の引き上げ

--- MyApp.java
+++ MyApp.java
@@ -2,7 +2,9 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    int r = new java.util.Random().nextInt(90);
-    System.out.println("あなたは多分、" + r + "歳ですね？");
+    int x = 5;
+    int y = 10;
+    String ans = "x+yは" + (x + y);
+    System.out.println(ans);
   }
 }
