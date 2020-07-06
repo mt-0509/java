1f78a2cb175841f2cc3566a9e4733c8e7b5e1c54
vagrant <vagrant@localhost.localdomain>
2020-07-06 21:21:42 +0900

    {}の省略について

--- MyApp.java
+++ MyApp.java
@@ -2,11 +2,13 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    boolean doorClose = true;
-    while (doorClose == true) {
-      System.out.println("ノックする");
-      System.out.println("1分間待つ");
-    }
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩に行きます");
+    } else // 1行しかないので{}を省略可能
+           // しかし実際の開発現場では省略しない方がいい
+      System.out.println("DVDを見ます");
     
   }
 }
