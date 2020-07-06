0987439a00f972d55eaea0007db6ca50a27ce54d
vagrant <vagrant@localhost.localdomain>
2020-07-06 09:28:52 +0900

    練習

--- MyApp.java
+++ MyApp.java
@@ -2,7 +2,16 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    boolean tenki = true;
+    int num = new java.util.Random().nextInt(3);
+    boolean tenki;
+
+    if (num == 1) {
+      tenki = true;
+    } else { 
+      tenki = false;
+    }
+
+
     if (tenki == true) {
       System.out.println("洗濯をします");
       System.out.println("散歩に行きます");
