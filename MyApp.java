2766e57eb3fd090ff6d40be6458587dfe58a3640
vagrant <vagrant@localhost.localdomain>
2020-07-06 21:13:35 +0900

    繰り返し処理

--- MyApp.java
+++ MyApp.java
@@ -2,21 +2,10 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    int num = new java.util.Random().nextInt(3);
-    boolean tenki;
-
-    if (num == 1) {
-      tenki = true;
-    } else { 
-      tenki = false;
-    }
-
-
-    if (tenki == true) {
-      System.out.println("洗濯をします");
-      System.out.println("散歩に行きます");
-    } else {
-      System.out.println("DVDを見ます");
+    boolean doorClose = true;
+    while (doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分間待つ");
     }
     
   }
