ad40b707617174c8b580b162d89927031081a627
vagrant <vagrant@localhost.localdomain>
2020-07-07 17:29:26 +0900

    数あてゲーム

--- MyApp.java
+++ MyApp.java
@@ -2,21 +2,18 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    System.out.print("[メニュー]1:検索2:登録3:削除4:変更");
-    int selected = new java.util.Scanner(System.in).nextInt();
-    switch (selected) {
-      case 1:
-        System.out.println("検索します");
+    System.out.println("【数あてゲーム】");
+    int ans = new java.util.Random().nextInt(10);
+    for (int i = 0; i < 5; i++) {
+      System.out.println("0~9の数字を入力してください");
+      int num = new java.util.Scanner(System.in).nextInt();
+      if (num == ans) {
+        System.out.println("アタリ！");
         break;
-      case 2:
-        System.out.println("登録します");
-        break;
-      case 3:
-        System.out.println("削除します");
-        break;
-      case 4:
-        System.out.println("変更します");
-        break;
+      } else {
+        System.out.println("違います");
+      }
     }
+    System.out.println("ゲームを終了します");
   }
 }
