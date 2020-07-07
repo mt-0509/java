ed4a9d0aa3724d873f974122b2c9c340b776f03a
vagrant <vagrant@localhost.localdomain>
2020-07-07 15:48:27 +0900

    for文

--- MyApp.java
+++ MyApp.java
@@ -3,15 +3,8 @@ public class MyApp {
 
   public static void main(String[] args) {
     System.out.println("あなたの運勢を占います");
-    int fortune = new java.util.Random().nextInt(4) + 1;
-    if (fortune == 1) {
-      System.out.println("大吉");
-    } else if (fortune == 2) {
-      System.out.println("中吉");
-    } else if (fortune == 3) {
-      System.out.println("吉");
-    } else {
-      System.out.println("凶");
+    for (int i = 0; i < 10; i++) {
+      System.out.println("こんにちは");
     }
   }
 }
