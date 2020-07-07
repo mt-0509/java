2f083049a4ad45be1330a994b5285721e4b10e9f
vagrant <vagrant@localhost.localdomain>
2020-07-07 18:33:29 +0900

    配列と繰り返し文

--- MyApp.java
+++ MyApp.java
@@ -2,7 +2,9 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    int[] scores1 = new int[] {20,30,40,50,80};
-    int[] scores2 = {20,30,40,50,80};
+    int [] scores = {20, 30, 40, 50, 80};
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println(scores[i]);
+    }
   }
 }
