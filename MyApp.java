5c386e0183e772896640b0013ada07b5ed58f516
vagrant <vagrant@localhost.localdomain>
2020-07-07 16:02:35 +0900

    for文を使った九九

--- MyApp.java
+++ MyApp.java
@@ -2,9 +2,12 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    System.out.println("あなたの運勢を占います");
-    for (int i = 0; i < 10; i++) {
-      System.out.println("こんにちは");
+    for (int i = 1; i < 10; i++) {
+      for (int j = 1; j < 10; j++) {
+        System.out.print(i * j);
+        System.out.print(" ");
+      }
+      System.out.println("");
     }
   }
 }
