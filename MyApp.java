6dd6066932d79f74fedec46572901a350ef7e98f
vagrant <vagrant@localhost.localdomain>
2020-07-07 17:57:16 +0900

    配列の短縮記法

--- MyApp.java
+++ MyApp.java
@@ -2,7 +2,7 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    int [] scores = new int[5];
-    System.out.println(scores[2]);
+    int[] scores1 = new int[] {20,30,40,50,80};
+    int[] scores2 = {20,30,40,50,80};
   }
 }
