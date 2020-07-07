7fb701b4cb31bd13cdf75f1514762bfd0e677e24
vagrant <vagrant@localhost.localdomain>
2020-07-07 17:18:44 +0900

    練習問題

--- MyApp.java
+++ MyApp.java
@@ -2,15 +2,21 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    int isHungry = 1;
-    String food = "唐揚げ";
-    System.out.println("こんにちは");
-    if (isHungry == 0) {
-      System.out.println("お腹がいっぱいです");
-    } else {
-      System.out.println("はらぺこです");
-      System.out.println(food + "をいただきます");
-      System.out.println("ごちそうさまでした");
+    System.out.print("[メニュー]1:検索2:登録3:削除4:変更");
+    int selected = new java.util.Scanner(System.in).nextInt();
+    switch (selected) {
+      case 1:
+        System.out.println("検索します");
+        break;
+      case 2:
+        System.out.println("登録します");
+        break;
+      case 3:
+        System.out.println("削除します");
+        break;
+      case 4:
+        System.out.println("変更します");
+        break;
     }
   }
 }
