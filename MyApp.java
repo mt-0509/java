eff8ef4e9acb09ef2915fb14a9b91b5affc10c94
vagrant <vagrant@localhost.localdomain>
2020-07-07 16:15:27 +0900

    条件式練習問題

--- MyApp.java
+++ MyApp.java
@@ -2,12 +2,9 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    for (int i = 1; i < 10; i++) {
-      for (int j = 1; j < 10; j++) {
-        System.out.print(i * j);
-        System.out.print(" ");
-      }
-      System.out.println("");
-    }
+    weight == 60;
+    (age1 + age2) * 2 > 60;
+    age % 2 == 1;
+    name.equals("湊");
   }
 }
