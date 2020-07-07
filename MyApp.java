a86d0105d70f67e6c67ada39539f625de6684cb5
vagrant <vagrant@localhost.localdomain>
2020-07-07 19:29:47 +0900

    拡張for文

--- MyApp.java
+++ MyApp.java
@@ -2,11 +2,9 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    int[] seq = new int[10];
-    for (int i = 0; i < 10; i++) {
-      seq[i] = new java.util.Random().nextInt(4);
-      char[] base = {'A', 'T', 'G', 'C'};
-      System.out.print(base[seq[i]] + " ");
+    int[] scores = {20, 30, 40, 50, 80};
+    for (int value : scores) {
+      System.out.println(value);
     }
   }
 }
