aa68fff9b5c9cad8fa0013cfea31c1dd49607537
vagrant <vagrant@localhost.localdomain>
2020-07-08 10:44:10 +0900

    nullについて

--- MyApp.java
+++ MyApp.java
@@ -2,9 +2,8 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    boolean judge = true;
-    if (judge == true) {
-      int[] array = {1,2,3};
-    }
+    int[] array = {1,2,3};
+    array = null;
+    array[0] = 10;
   }
 }
