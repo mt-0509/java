228eee15bca541dc4ffa4c16a08e2d6ac745c40a
vagrant <vagrant@localhost.localdomain>
2020-07-08 12:04:02 +0900

    配列作成練習問題

--- MyApp.java
+++ MyApp.java
@@ -2,8 +2,9 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    int [] [] scores = {{40,50,60},{80,60,70}};
-    System.out.println(scores.length); // 2が出力される
-    System.out.println(scores[0].length); // 3が出力される
+    int[] points = new int[4];
+    double[] weights = new double[5];
+    boolean[] answers = new boolean[3];
+    String[] names = new String[3];
   }
 }
