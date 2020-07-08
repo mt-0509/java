3693bf27d2b169bd82b2ced8076f72cb0c50817e
vagrant <vagrant@localhost.localdomain>
2020-07-08 16:56:46 +0900

    複数の引数を渡す

--- MyApp.java
+++ MyApp.java
@@ -2,14 +2,12 @@
 public class MyApp {
 
   public static void main(String[] args) {
-    System.out.println("メソッドを呼び出します");
-    System.out.println("名前を教えてください");
-    String userName = new java.util.Scanner(System.in).nextLine();
-    hello(userName);
-    System.out.println("メソッドの呼び出しが終わりました");
+    add(100,20);
+    add(200,50);
   }
 
-  public static void hello(String name) {
-    System.out.println(name + "さん、こんにちは");
+  public static void add(int x, int y) {
+    int ans = x + y;
+    System.out.println(x + "+" + y + "=" + ans);
   }
 }
