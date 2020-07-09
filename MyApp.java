dd4f87d59e9435daa793cd0e1c33296c2f40a443
vagrant <vagrant@localhost.localdomain>
2020-07-09 09:02:43 +0900

    オーバーロード

--- MyApp.java
+++ MyApp.java
@@ -5,8 +5,20 @@ public class MyApp {
     int ans = x + y;
     return ans;
   }
+
+  public static double add(double x, double y) {
+    double ans = x + y;
+    return ans;
+  }
+
+  public static String add(String x, String y) {
+    String ans = x + y;
+    return ans;
+  }
   
   public static void main(String[] args) {
-    System.out.println(add(add(10,20),add(30,40)));
+    System.out.println(add(10,20));
+    System.out.println(add(3.5,2.7));
+    System.out.println(add("Hello","World"));
   }
 }
