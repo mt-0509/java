4be84d95d749a16c29b34d60e04ff1ad77d44a62
vagrant <vagrant@localhost.localdomain>
2020-07-09 13:53:12 +0900

    引数の数が違うオーバーロード

--- MyApp.java
+++ MyApp.java
@@ -1,24 +1,16 @@
 
 public class MyApp {
-  
-  public static int add(int x, int y) {
-    int ans = x + y;
-    return ans;
+
+  public static int add(int x,int y) {
+    return x + y;
   }
 
-  public static double add(double x, double y) {
-    double ans = x + y;
-    return ans;
-  }
-
-  public static String add(String x, String y) {
-    String ans = x + y;
-    return ans;
+  public static int add(int x, int y, int z) {
+    return x + y + z;
   }
   
   public static void main(String[] args) {
-    System.out.println(add(10,20));
-    System.out.println(add(3.5,2.7));
-    System.out.println(add("Hello","World"));
+    System.out.println("10+20=" + add(10,20));
+    System.out.println("10+20+30=" + add(10,20,30));
   }
 }
