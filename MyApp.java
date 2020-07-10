df429b20888d7a61c42693dcbfb2aca04e13cd92
vagrant <vagrant@localhost.localdomain>
2020-07-10 10:21:45 +0900

    練習問題

--- MyApp.java
+++ MyApp.java
@@ -1,18 +1,15 @@
 
 public class MyApp {
 
-  public static int[] makeArray(int size) {
-    int[] newArray = new int[size];
-    for (int i = 0; i < newArray.length; i++) {
-      newArray[i] = i;
-    }
-    return newArray;
+  public static void introduceOneself() {
+    String name = "とし";
+    int age = 27;
+    double height = 174.2;
+    char eto = '鳥';
+    System.out.println("私の名前は" + name + "です。" + "年齢は" + age + "歳で、身長は" + height + "センチです。" + "干支は" + eto + "です。よろしくお願いします。");
   }
   
   public static void main(String[] args) {
-    int[] array = makeArray(3);
-    for (int i : array) {
-      System.out.println(i);
-    }
+    introduceOneself();
   }
 }
