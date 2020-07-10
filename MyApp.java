9e5f370b997e48320bd52b1a36f9504d89f1e506
vagrant <vagrant@localhost.localdomain>
2020-07-10 10:40:09 +0900

    練習問題

--- MyApp.java
+++ MyApp.java
@@ -1,15 +1,16 @@
 
 public class MyApp {
 
-  public static void introduceOneself() {
-    String name = "とし";
-    int age = 27;
-    double height = 174.2;
-    char eto = '鳥';
-    System.out.println("私の名前は" + name + "です。" + "年齢は" + age + "歳で、身長は" + height + "センチです。" + "干支は" + eto + "です。よろしくお願いします。");
+  public static void email(String title, String address, String text) {
+    System.out.println(address + "に以下のメールを送信しました");
+    System.out.println("件名:" + title);
+    System.out.println("本文:" + text);
   }
   
   public static void main(String[] args) {
-    introduceOneself();
+    String title = "明日の件";
+    String address = "yahoo.co.jp";
+    String text = "よろしく頼むわー";
+    email(title, address, text);
   }
 }
