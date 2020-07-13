818fb680c6c8b01a7723baca555f01d867f61c56
vagrant <vagrant@localhost.localdomain>
2020-07-14 00:02:31 +0900

    クラス

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
-import comment.Zenhan;
-
 public class Main {
-
-  public static void main (String[] args) throws Exception {
-    Zenhan.doWarusa();
-    Zenhan.doTogame();
-    comment.Kouhan.callDeae();
-    comment.Kouhan.showMondokoro();
+  public static void main(String[] args) {
+    Hero h = new Hero();
+    h.name = "ミナト";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました！");
+    h.sit(5);
+    h.slip();
+    h.sit(25);
+    h.run();
   }
-  
-  
 }
