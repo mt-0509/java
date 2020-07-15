3f763af0304170350fe53e450281479ebae4a4c4
vagrant <vagrant@localhost.localdomain>
2020-07-15 19:43:29 +0900

    フィールドでクラスを使う

--- Main.java
+++ Main.java
@@ -1,25 +1,12 @@
 public class Main {
   public static void main(String[] args) {
+    Sword s = new Sword();
+    s.name = "炎の剣";
+    s.damage = 10;
     Hero h = new Hero();
     h.name = "ミナト";
     h.hp = 100;
-    
-    Matango m1 = new Matango();
-    m1.hp = 50;
-    m1.suffix = 'A';
-
-    Matango m2 = new Matango();
-    m2.hp = 48;
-    m2.suffix = 'B';
-
-    Cleric c = new Cleric();
-    c.name = "おっさん";
-    c.selfAid();
-    c.pray(2);
-
-    h.slip();
-    m1.run();
-    m2.run();
-    h.run();
+    h.sword = s;
+    System.out.println("現在の武器は" + h.sword.name);
   }
 }
