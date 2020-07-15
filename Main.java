904a705e1c7bc1ce5798ff19c22315956a6832b7
vagrant <vagrant@localhost.localdomain>
2020-07-15 19:50:18 +0900

    練習問題

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 public class Main {
+
   public static void main(String[] args) {
     Sword s = new Sword();
     s.name = "炎の剣";
@@ -8,5 +9,6 @@ public class Main {
     h.hp = 100;
     h.sword = s;
     System.out.println("現在の武器は" + h.sword.name);
+    h.attack();
   }
 }
