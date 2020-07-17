11047a05ae8da77fe21f037680fc8fba15e514a6
vagrant <vagrant@localhost.localdomain>
2020-07-17 22:47:34 +0900

    コンストラクタに引数を渡す

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
   public static void main(String[] args) {
-    Hero h = new Hero();
+    Hero h = new Hero("ミナト");
 
     System.out.println(h.hp);
+    System.out.println(h.name);
   }
 }
