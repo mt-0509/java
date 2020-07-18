9f90ba92ee3a16d5fbbc6930311b0a6c3e0ac909
vagrant <vagrant@localhost.localdomain>
2020-07-18 13:47:09 +0900

    コンストラクタ練習問題

--- Main.java
+++ Main.java
@@ -5,5 +5,15 @@ public class Main {
     System.out.println(h1.name);
     Hero h2 = new Hero();
     System.out.println(h2.name);
+
+    Thief t1 = new Thief("アサカ", 40, 5);
+    System.out.println(t1.name + t1.hp + "と" + t1.mp);
+
+    Thief t2 = new Thief("田中",100);
+    System.out.println("俺の名前は" + t2.name + "だ！" + "HPは" + t2.hp + " MPは" + t2.mp);
+
+    Thief t3 = new Thief("伊藤");
+    System.out.println("俺の名前は" + t3.name + "だ！" + "HPは" + t3.hp + " MPは" + t3.mp);
+
   }
 }
