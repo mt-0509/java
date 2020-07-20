a0e413d3d43c67c2d03ad79da38982a80d8faeef
vagrant <vagrant@localhost.localdomain>
2020-07-20 13:44:10 +0900

    メソッドの継承を禁止する

--- Hero.java
+++ Hero.java
@@ -2,6 +2,11 @@ public class Hero {
   String name = "ミナト";
   int hp = 100;
 
+  public final void slip() {
+    this.hp -= 5;
+    System.out.println(this.name + "は転んだ！");
+    System.out.println("5のダメージ");
+  }
   public void attack(Matango m) {
     System.out.println(this.name + "の攻撃！");
     m.hp -= 5;
