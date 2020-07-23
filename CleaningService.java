fe07ffb3a7d958cc7119a7b2e7920bbea890eb47
vagrant <vagrant@localhost.localdomain>
2020-07-23 11:16:21 +0900

    インターフェース

---
+++ CleaningService.java
@@ -0,0 +1,5 @@
+public interface CleanigService {
+  Shirt washShirt(Shirt s);
+  Towl washTowl(Towl t);
+  Coat washCoat(Coat c);
+}
--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
 
   public static void main(String[] args) {
-    Hero h = new Hero();
-    PoisonMatango pm = new PoisonMatango('A');
-    System.out.println(pm.suffix);
+    
   }
 }
