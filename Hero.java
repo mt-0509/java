bc83d8349bf12e607002d5097b25e9eaa328b44b
vagrant <vagrant@localhost.localdomain>
2020-07-28 18:48:01 +0900

    カプセル化の仕方

--- Hero.java
+++ Hero.java
@@ -1,7 +1,14 @@
 public class Hero {
-  int hp;
+  private int hp;
+  String name;
+  Sword sword;
 
-  public Hero() {
-    System.out.println("Heroのコンストラクタが動作");
+  public void sleep() {
+    this.hp = 100;
+    System.out.println(this.name + "は、眠って回復した！");
+  }
+  private void die() {
+    System.out.println(this.name + "は死んでしまった！");
+    System.out.println("GAME OVERです。");
   }
 }
--- Sword.java
+++ Sword.java
@@ -1,4 +1,3 @@
 public class Sword {
   String name;
-  int damage;
 }
