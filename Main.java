3bad93b24952eeb3f60fdff63e272735b9c79791
vagrant <vagrant@localhost.localdomain>
2020-07-30 21:40:43 +0900

    どんなインスタンスでも受け取れるメソッド

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
-
+  public static void printAnything(Object o) {
+    System.out.println(o.toString());
+  }
   public static void main(String[] args) {
-    Object o1 = new Empty();
-    Object o2 = new Hero();
-    Object o3 = "こんにちは";
-    System.out.println(o3);
+    printAnything("やったー");
   }
 }
