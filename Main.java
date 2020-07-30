fc901758f9fed3183fbe08cf01efcba1a7af7b78
vagrant <vagrant@localhost.localdomain>
2020-07-30 21:27:55 +0900

    Objectクラスについて

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
   public static void main(String[] args) {
-    Empty e = new Empty();
-    String s = e.toString();
-    System.out.println(s);
+    Object o1 = new Empty();
+    Object o2 = new Hero();
+    Object o3 = "こんにちは";
+    System.out.println(o3);
   }
 }
