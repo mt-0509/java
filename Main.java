b4d32fc453c5e6967c69536cab165565d77ca989
vagrant <vagrant@localhost.localdomain>
2020-08-03 17:41:53 +0900

    書式を指定して表示する

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    String s = "abc,def:ghi";
-    String w = s.replaceAll("[beh]","X");
-    System.out.println(w);
+    System.out.printf("製品番号%s-%02d","SVJ",3);
   }
 }
