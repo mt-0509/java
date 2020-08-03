dd212b0719bd587883c405dcafdcb815eee1e233
vagrant <vagrant@localhost.localdomain>
2020-08-03 15:43:46 +0900

    文字列の分割

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    // public boolean isValidPlayerName(String name) {
-    //   return name.matches("[A-Z][A-Z0-9]{7}");
-    // }
-    Hero h = new Hero();
-
-    String s = "A0000203";
-    boolean ans = h.isValidPlayerName(s);
-    System.out.println(ans);
+    String s = "abc,def:ghi";
+    String[] words = s.split("[,:]");
+    for (String w : words) {
+      System.out.print(w + "->");
+    }
   }
 }
