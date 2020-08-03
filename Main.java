222fd9864a8d5df04171262899fcfd49be44c620
vagrant <vagrant@localhost.localdomain>
2020-08-03 17:55:34 +0900

    日付 long型

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.printf("製品番号%s-%02d","SVJ",3);
+    long start = System.currentTimeMillis();
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 10000; i++) {
+      sb.append("Java");
+    }
+    String s = sb.toString();
+    String yahoo = "yahoo" + "yahoo";
+    System.out.println(s);
+    System.out.println(yahoo);
+
+    long end = System.currentTimeMillis();
+    System.out.println("処理にかかった時間は…" + (end - start) + "ミリ秒でした");
   }
 }
