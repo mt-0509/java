8627add13cbafd48af27ea507929c63d3a2b9aac
vagrant <vagrant@localhost.localdomain>
2020-08-04 16:54:15 +0900

    現在日時を表示する

--- Main.java
+++ Main.java
@@ -1,17 +1,11 @@
+import java.util.Date;
+
 public class Main {
   public static void main(String[] args) {
-    long start = System.currentTimeMillis();
-
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < 10000; i++) {
-      sb.append("Java");
-    }
-    String s = sb.toString();
-    String yahoo = "yahoo" + "yahoo";
-    System.out.println(s);
-    System.out.println(yahoo);
-
-    long end = System.currentTimeMillis();
-    System.out.println("処理にかかった時間は…" + (end - start) + "ミリ秒でした");
+    Date now = new Date();
+    System.out.println(now);
+    System.out.println(now.getTime());
+    Date past = new Date(1600705425827L);
+    System.out.println(past);
   }
 }
