e1d402946bfd727f0e760bd4a3e6623cd10a5b37
vagrant <vagrant@localhost.localdomain>
2020-08-06 15:31:48 +0900

    ラッパークラスをArrayListに格納

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
-import java.util.Date;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-    // 文字列からDateインスタンスを生成
-    Date d = f.parse("2020/09/22 01:23:45");
-    System.out.println(d);
-    // Dateインスタンスから文字列を生成
-    Date now = new Date();
-    String s = f.format(now);
-    System.out.println("現在は" + s + "です");
+    ArrayList<Integer> points = new ArrayList<Integer>();
+    points.add(10);
+    points.add(80);
+    points.add(75);
+    for (int i : points) {
+      System.out.println(i);
+    }
   }
 }
